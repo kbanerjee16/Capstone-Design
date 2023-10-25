af52f6b9796252b5b28e88fd1703029f77e26e90
kbanerjee16 <89931283+kbanerjee16@users.noreply.github.com>
2023-10-25 10:47:31 -0400

    pausing work on this file for now, does not work

--- Step 3-Crop Video/crop_video.py
+++ Step 3-Crop Video/crop_video.py
@@ -2,14 +2,38 @@ import cv2
 
 # Function to crop black pixels from a frame
 def crop_black_pixels(frame):
-    # (Your cropping function remains the same)
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Create a mask for black pixels (threshold for low-intensity values)
+    mask = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)[1]
+
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Initialize an empty list to store the cropped regions
+    cropped_regions = []
+
+    # Iterate through the contours and crop non-black regions
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if w > 10 and h > 10:  # Define a minimum size for the region to be considered
+            cropped_region = frame[y:y+h, x:x+w]
+            cropped_regions.append(cropped_region)
+
+    return cropped_regions
 
 # Open the video file
-video_capture = cv2.VideoCapture('no_background_video.mp4')
+video_capture = cv2.VideoCapture('output_video.avi')
+
+# Get video frame dimensions
+frame_width = int(video_capture.get(3))
+frame_height = int(video_capture.get(4))
 
 # Create a VideoWriter object to save the output video with 'mp4v' codec
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_video = cv2.VideoWriter('cropped_video.mp4', fourcc, 30, (640, 480))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_video = 'cropped_video.avi'
+out = cv2.VideoWriter(output_video, fourcc, 30, (frame_width, frame_height), isColor=True)
 
 while True:
     ret, frame = video_capture.read()
@@ -26,10 +50,10 @@ video_capture.release()
 output_video.release()
 
 # Open the cropped video for playing
-cropped_video_capture = cv2.VideoCapture('cropped_video.mp4')
+#cropped_video_capture = cv2.VideoCapture('cropped_video.mp4')
 
 while True:
-    ret, frame = cropped_video_capture.read()
+    ret, frame = output_video.read()
     if not ret:
         break
 
